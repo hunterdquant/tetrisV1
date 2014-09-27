d13e136069dc7e02d233ba12cc411ff914e6c266
Hunter Quant <hunterdquant@gmail.com>
2014-09-27 15:09:56 -0400

    Added template for drawing cells

--- Tetris.java
+++ Tetris.java
@@ -17,15 +17,20 @@ public class Tetris extends JPanel {
         this.setPreferredSize(new java.awt.Dimension(640, 480));
         this.setBackground(Color.GREEN);
      }
+
+     public void drawCell(Graphics g, int x, int y) {
+     
+         g.setColor(Color.BLACK);
+         g.fillRect(x, y, 24, 24);
+         g.setColor(Color.RED);
+         g.fillRect(x+1, y+1, 22, 22);
+     }
      
      //Draws a red cell with a black border
      public void paint(Graphics g) {
         
          super.paint(g);
-         g.setColor(Color.BLACK);
-         g.fillRect(0, 0, 24, 24);
-         g.setColor(Color.RED);
-         g.fillRect(1, 1, 22, 22);
+         drawCell(g, 0, 0);
      }
 }
 
